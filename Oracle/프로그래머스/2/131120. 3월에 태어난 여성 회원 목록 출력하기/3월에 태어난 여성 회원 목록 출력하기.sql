361ac8d71c7e40a97c335e0d4d609ac9bab04574
Pinche-Guapo <98554153+Pinche-Guapo@users.noreply.github.com>
2026-04-01 17:13:11 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ Oracle/프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT member_id, member_name, gender, to_char(date_of_birth, 'YYYY-MM-DD') from member_profile where TO_CHAR(DATE_OF_BIRTH, 'MM') = '03' and tlno is not null and gender = 'W' order by member_id asc;
